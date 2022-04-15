4e0f1954933a67f1d5d26904d90190572cfcce49
Clayton <claytonsilvar@hotmail.com>
2022-04-15 11:41:22 -0300

    realocando arquivos e executavel criado

---
+++ src/data_base.py
@@ -0,0 +1,74 @@
+import requests
+import json
+a = open("..\keys\mongodbKey.txt","r")
+apiKey = a.read()
+a.close()
+headers = {
+        'Content-Type': 'application/json',
+        'Access-Control-Request-Headers': '*',
+        'api-key': apiKey
+    }
+url = "https://data.mongodb-api.com/app/data-guzuj/endpoint/data/beta/action/"
+
+def insertOne(userId, userName, userTrackingNumber):
+    url = "https://data.mongodb-api.com/app/data-guzuj/endpoint/data/beta/action/insertOne"
+    payload = json.dumps({
+        "dataSource": "RochaESilvaDB",
+        "database": "Distribuidora",
+        "collection": "dboUsuario",
+        "document": {
+            "_id": userId, 
+            "user_name": userName,
+            "user_tracking_number": [userTrackingNumber],
+        }
+    })
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+def findOne(field, value):
+    url = "https://data.mongodb-api.com/app/data-guzuj/endpoint/data/beta/action/findOne"
+    
+    if(field == "user_tracking_number"):
+        payload = json.dumps({
+            "collection": "dboUsuario",
+            "database": "Distribuidora",
+            "dataSource": "RochaESilvaDB",
+            "filter": {
+                "_id":value[1],
+                field: value[0]
+            }
+        })
+    else:    
+        payload = json.dumps({
+            "collection": "dboUsuario",
+            "database": "Distribuidora",
+            "dataSource": "RochaESilvaDB",
+            "filter": {
+                field: value
+            }
+        })
+    response = requests.request("POST", url, headers=headers, data=payload)
+    resp = response.json()
+    if(resp["document"] is None):
+        return None
+    else:
+        return resp["document"]
+
+def updateOne(userId: int, userTrackingNumber: int):
+    jsonUpdate = findOne("_id",userId)
+    jsonUpdate["user_tracking_number"].append(userTrackingNumber)
+    url = "https://data.mongodb-api.com/app/data-guzuj/endpoint/data/beta/action/updateOne"
+
+    payload = json.dumps({
+    "collection": "dboUsuario",
+    "database": "Distribuidora",
+    "dataSource": "RochaESilvaDB",
+    "filter": {"_id": userId},
+    "update": {
+          "$set": {
+              "user_tracking_number": jsonUpdate["user_tracking_number"],
+          }
+      }
+    })
+    response = requests.request("POST", url, headers=headers, data=payload)
+    resp = response.json()    
+    return resp
---
+++ src/get_json_request.py
@@ -0,0 +1,110 @@
+import requests
+import json
+import datetime
+import locale
+#from flask import Flask, request
+from Object import Object
+from Event import Event
+#from waitress import serve
+
+#app = Flask(__name__)
+
+def initializeObject(data):
+    try:
+        object = Object("200",data['objetos'][0]['codObjeto'],
+            None,
+            data['objetos'][0]['tipoPostal']['categoria'],
+            data['objetos'][0]['tipoPostal']['descricao'],
+            data['objetos'][0]['modalidade'],
+            None
+        )
+    except:
+        object = Object(200,data['objetos'][0]['codObjeto'],
+            data['objetos'][0]['dtPrevista'],
+            data['objetos'][0]['tipoPostal']['categoria'],
+            data['objetos'][0]['tipoPostal']['descricao'],
+            data['objetos'][0]['modalidade'],
+            None
+        )
+    return object
+
+
+
+def getJsonRequest(key):
+    url = "https://proxyapp.correios.com.br/v1/sro-rastro/"
+    url += key
+    r = requests.get(url)
+    data = r.json()
+    return data
+
+def addAllEvents(data, object):
+    for x in data['objetos'][0]['eventos']:
+        formattedDate = datetime.datetime.strptime(x['dtHrCriado'],'%Y-%m-%dT%H:%M:%S')
+        if(data['objetos'][0]['modalidade'] == "V" and x['codigo'] != "PO" and x['codigo'] != "OEC" and x['codigo'] != "BDE"):
+            object.addEvents(
+                x['descricao'],
+                formattedDate.strftime("%A, %d. %B %Y %I:%M%p").capitalize(),
+                x['unidade']['tipo'],
+                None,
+                x['unidade']['nome'],
+                x['unidadeDestino']['tipo'],
+                x['unidadeDestino']['endereco']['uf'],
+                x['unidadeDestino']['nome'])
+        elif(data['objetos'][0]['modalidade'] == "V"):
+            object.addEvents(
+                x['descricao'],
+                formattedDate.strftime("%A, %d. %B %Y %I:%M%p").capitalize(),
+                x['unidade']['tipo'],
+                None,
+                x['unidade']['nome'],
+                None,
+                None,
+                None)            
+        elif(data['objetos'][0]['modalidade'] != "V" and x['codigo'] != "PO" and x['codigo'] != "OEC" and x['codigo'] != "BDE"):
+            object.addEvents(
+                x['descricao'],
+                formattedDate.strftime("%A, %d. %B %Y %I:%M%p").capitalize(),
+                x['unidade']['endereco']['cidade'],
+                x['unidade']['endereco']['uf'],
+                x['unidade']['tipo'],
+                x['unidadeDestino']['endereco']['cidade'],
+                x['unidadeDestino']['endereco']['uf'],
+                x['unidadeDestino']['tipo'])
+        else:
+            object.addEvents(
+                x['descricao'],
+                formattedDate.strftime("%A, %d. %B %Y %I:%M%p").capitalize(),
+                x['unidade']['endereco']['cidade'],
+                x['unidade']['endereco']['uf'],
+                x['unidade']['tipo'],
+                None,
+                None,
+                None)
+                    
+
+def exportJson(object):
+    with open('data.json', 'w',encoding='utf8') as f:
+        json.dump(object,f,ensure_ascii=False,default=lambda o: o.__dict__)
+
+
+#@app.route("/rastrear", methods=["GET"])
+def rastreio(id):
+    # f = open("data.json",encoding='utf8')
+    # data = json.load(f)
+    # return data
+    #id = request.args.get("id")
+    locale.setlocale(locale.LC_ALL, 'pt_pt.UTF-8')
+    data = getJsonRequest(id)
+    try:
+        object = initializeObject(data)
+        addAllEvents(data, object)
+        #object.events.invert()
+        #object.printList()
+        #exportJson(object)
+        return json.dumps(object,ensure_ascii=False,default=lambda o: o.__dict__)
+    except:
+        return json.dumps({"cod":"404","mensagem":" SRO-019: Objeto inválido"},ensure_ascii=False,default=lambda o: o.__dict__)
+
+#if __name__ == '__main__':
+    #app.run()
+    #serve(app, port=8080, host="0.0.0.0")
